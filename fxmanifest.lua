3512a100683c2561ee8ea5de6af7027fabad08e0
CristianoTRS <50265039+CristianoTRS@users.noreply.github.com>
2020-08-16 13:57:37 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+ui_page 'html/index.html'
+
+client_scripts {
+    'config.lua',
+    'client/functions.lua',
+    'client/client.lua'
+}
+
+server_script 'server/server.lua'
+
+files {
+    'html/*'
+}
