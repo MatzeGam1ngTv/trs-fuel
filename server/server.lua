52dd05d056fcdca619d36df57aea4c2a8e4d5fe7
MatzeGam1ngTv <club19457@gmail.com>
2020-08-17 01:45:30 +0200

    Update server.lua
    Edit

--- server/server.lua
+++ server/server.lua
@@ -2,7 +2,7 @@ ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-ESX.RegisterServerCallback('ug-fuel:checkMoney', function(source, cb, data)
+ESX.RegisterServerCallback('trs-fuel:checkMoney', function(source, cb, data)
 
     local xPlayer = ESX.GetPlayerFromId(source)
     local xPlayerMoney = xPlayer.getMoney()
